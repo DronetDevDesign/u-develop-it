9d6e07dbdbe2a65b164c86ad28cf78bc535faf3b
Ron Dronet <ron@dronetdevdesign.com>
2022-08-10 17:05:31 -0500

    revised code files by adding apiRoute folder containing route files

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+// Add near the top of the file
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+
+
+module.exports = router;
